1f22c87a9d58455fe6b8d1ac2abee00ecb362ac1
Alex Clements <alexfjclements@gmail.com>
2018-11-21 13:29:47 +0000

    first two tests passed

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,18 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.filmTitles = function () {
+  let result = this.films.map((film) => {
+    return film.title;
+  })
+  return result;
+};
+
+Cinema.prototype.findFilmByTitle = function (title) {
+  let result = this.films.find((film) => {
+    return film.title === title;
+  });
+  return result;
+};
+
 module.exports = Cinema;
--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -28,8 +28,17 @@ describe('Cinema', function () {
     assert.deepStrictEqual(actual, films);
   });
 
-  it('should be able to get a list of film titles');
-  it('should be able to find a film by title');
+  it('should be able to get a list of film titles', function () {
+    const actual = cinema.filmTitles();
+    const titleList = ['Moonlight', 'Blade Runner 2049', 'Dunkirk', 'Black Panther', 'T2 Trainspotting']
+    assert.deepStrictEqual(actual, titleList);
+  });
+
+  it('should be able to find a film by title', function () {
+    const actual = cinema.findFilmByTitle(dunkirk.title);
+    const expected = dunkirk;
+    assert.strictEqual(actual, expected);
+  });
   it('should be able to filter films by genre');
   it('should be able to check whether there are some films from a particular year');
   it('should be able to check whether there are no films from a particular year');
